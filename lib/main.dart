bfa6263e1e8079b37e7ba5c3dcf3424bc2e0c7ba
CatNighp <CatNighp@gmail.com>
2023-06-09 21:00:43 +0900

    session1

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,93 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final deviceSize = MediaQuery.of(context).size;
+    final deviceSizePlaceholder = deviceSize.width / 2;
+    final deviceSizeText = deviceSizePlaceholder / 2;
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: deviceSizePlaceholder,
+                    height: deviceSizePlaceholder,
+                    child: Placeholder(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        alignment: Alignment.center,
+                        width: deviceSizeText,
+                        child: Text(
+                          '**℃',
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge
+                              ?.copyWith(color: Colors.blue),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        alignment: Alignment.center,
+                        width: deviceSizeText,
+                        child: Text(
+                          '**℃',
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge
+                              ?.copyWith(color: Colors.red),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 80,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: deviceSizeText,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Close',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge
+                            ?.copyWith(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: deviceSizeText,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Reload',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge
+                            ?.copyWith(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
