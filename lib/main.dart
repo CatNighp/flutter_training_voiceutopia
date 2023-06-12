dd8974240d982979c055d06004c98919c95e858b
CatNighp <CatNighp@gmail.com>
2023-06-12 17:55:35 +0900

    session2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final yumemiWeather = YumemiWeather();
+  late String weather;
+
+  @override
+  void initState() {
+    super.initState();
+    weather = '';
+  }
 
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     final deviceSizePlaceholder = deviceSize.width / 2;
     final deviceSizeText = deviceSizePlaceholder / 2;
+    final deviceSizeHeight = (deviceSize.height / 2) + deviceSizeText + 32;
+
     return MaterialApp(
       home: Scaffold(
         body: Stack(
@@ -20,16 +39,23 @@ class MainApp extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
+                  SizedBox(
                     width: deviceSizePlaceholder,
                     height: deviceSizePlaceholder,
-                    child: Placeholder(),
+                    child: SvgPicture.asset(
+                      'assets/images/$weather',
+                      width: deviceSizePlaceholder,
+                      height: deviceSizePlaceholder,
+                      placeholderBuilder: (context) {
+                        return const Placeholder();
+                      },
+                    ),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        padding: EdgeInsets.all(16),
+                        padding: const EdgeInsets.all(16),
                         alignment: Alignment.center,
                         width: deviceSizeText,
                         child: Text(
@@ -41,7 +67,7 @@ class MainApp extends StatelessWidget {
                         ),
                       ),
                       Container(
-                        padding: EdgeInsets.all(16),
+                        padding: const EdgeInsets.all(16),
                         alignment: Alignment.center,
                         width: deviceSizeText,
                         child: Text(
@@ -58,7 +84,7 @@ class MainApp extends StatelessWidget {
               ),
             ),
             Positioned(
-              bottom: 80,
+              top: deviceSizeHeight + 80,
               left: 0,
               right: 0,
               child: Row(
@@ -82,7 +108,11 @@ class MainApp extends StatelessWidget {
                     alignment: Alignment.center,
                     width: deviceSizeText,
                     child: TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          weather = '${yumemiWeather.fetchSimpleWeather()}.svg';
+                        });
+                      },
                       child: Text(
                         'Reload',
                         style: Theme.of(context)
