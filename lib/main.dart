c00f1cf7c55ace008d5d8dfc2b7c4cab90c33c52
CatNighp <31886522+CatNighp@users.noreply.github.com>
2023-07-03 21:21:51 +0900

    Revert "Session/8"

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,27 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_training/API/weather_api.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
-final weatherArrayProvider = Provider<StateController<List<dynamic>>>((ref) {
-  return ref.watch(weatherArrayStateProvider);
-});
-
-final weatherArrayStateProvider = StateProvider((ref) {
-  return StateController<List<dynamic>>(['', '**', '**']);
-});
-
-void updateArray(ProviderContainer container, String weatherData) {
-  final weatherMap =
-      WeatherApi.fromJson(jsonDecode(weatherData) as Map<String, dynamic>);
-
-  final weatherArray = container.read(weatherArrayProvider);
-  // ignore: cascade_invocations
-  weatherArray.state = [
-    weatherMap.weatherCondition,
-    weatherMap.maxTemperature,
-    weatherMap.minTemperature,
-  ];
-}
-
-const jsonString = '''
-{
-    "area": "tokyo",
-    "date": "2020-04-01T12:00:00+09:00"
-}''';
-
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MaterialApp(
-        home: GreenPage(),
-      ),
-    ),
-  );
+  runApp(const MainApp());
 }
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _MainAppState createState() => _MainAppState();
 }
 
 class _MainAppState extends State<MainApp> {
   final yumemiWeather = YumemiWeather();
   late String weather;
-  final container = ProviderContainer();
 
   @override
   void initState() {
     super.initState();
     weather = '';
-  }
-
-  @override
-  void dispose() {
-    container.dispose();
-    super.dispose();
   }
 
   @override
@@ -73,8 +31,8 @@ class _MainAppState extends State<MainApp> {
     final deviceSizeText = deviceSizePlaceholder / 2;
     final deviceSizeHeight = (deviceSize.height / 2) + deviceSizeText + 32;
 
-    return ProviderScope(
-      child: Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         body: Stack(
           children: [
             Center(
@@ -84,14 +42,16 @@ class _MainAppState extends State<MainApp> {
                   SizedBox(
                     width: deviceSizePlaceholder,
                     height: deviceSizePlaceholder,
+
                     child: SvgPicture.asset(
-                      'assets/images/${container.read(weatherArrayProvider).state[0]}.svg',
+                      'assets/images/$weather',
                       width: deviceSizePlaceholder,
                       height: deviceSizePlaceholder,
                       placeholderBuilder: (context) {
                         return const Placeholder();
                       },
                     ),
+
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -101,7 +61,7 @@ class _MainAppState extends State<MainApp> {
                         alignment: Alignment.center,
                         width: deviceSizeText,
                         child: Text(
-                          '${container.read(weatherArrayProvider).state[2]}℃',
+                          '**℃',
                           style: Theme.of(context)
                               .textTheme
                               .labelLarge
@@ -113,7 +73,7 @@ class _MainAppState extends State<MainApp> {
                         alignment: Alignment.center,
                         width: deviceSizeText,
                         child: Text(
-                          '${container.read(weatherArrayProvider).state[1]}℃',
+                          '**℃',
                           style: Theme.of(context)
                               .textTheme
                               .labelLarge
@@ -136,9 +96,7 @@ class _MainAppState extends State<MainApp> {
                     alignment: Alignment.center,
                     width: deviceSizeText,
                     child: TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+                      onPressed: () {},
                       child: Text(
                         'Close',
                         style: Theme.of(context)
@@ -154,36 +112,7 @@ class _MainAppState extends State<MainApp> {
                     child: TextButton(
                       onPressed: () {
                         setState(() {
-                          try {
-                            weather = yumemiWeather.fetchWeather(jsonString);
-                            updateArray(container, weather);
-                            // ignore: avoid_catches_without_on_clauses
-                          } catch (e) {
-                            // ignore: inference_failure_on_function_invocation
-                            showDialog(
-                              barrierDismissible: false,
-                              context: context,
-                              builder: (context) {
-                                return AlertDialog(
-                                  title: const Text('エラったよ'),
-                                  actions: [
-                                    GestureDetector(
-                                      child: Text(
-                                        'OK',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .labelLarge
-                                            ?.copyWith(color: Colors.blue),
-                                      ),
-                                      onTap: () {
-                                        Navigator.pop(context);
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
+                          weather = '${yumemiWeather.fetchSimpleWeather()}.svg';
                         });
                       },
                       child: Text(
@@ -202,42 +131,5 @@ class _MainAppState extends State<MainApp> {
         ),
       ),
     );
-  }
-}
-
-mixin GreenPageMixin<T extends StatefulWidget> on State<T> {
-  void navigatorGreen() {
-    Future.delayed(const Duration(milliseconds: 500), () {
-      Navigator.push(
-        context,
-        // ignore: inference_failure_on_instance_creation
-        MaterialPageRoute(builder: (context) => const MainApp()),
-      ).then((value) {
-        (context as Element).markNeedsBuild();
-      });
-    });
-  }
-
-  Widget buildGreenPage(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
-      body: Center(),
-    );
-  }
-}
-
-class GreenPage extends StatefulWidget {
-  const GreenPage({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _GreenPageState createState() => _GreenPageState();
-}
-
-class _GreenPageState extends State<GreenPage> with GreenPageMixin<GreenPage> {
-  @override
-  Widget build(BuildContext context) {
-    navigatorGreen();
-    return buildGreenPage(context);
   }
 }
